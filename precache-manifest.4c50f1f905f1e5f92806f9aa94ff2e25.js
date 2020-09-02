59f28bb1ffbc2a2cebe4dbd2d991babb9a0d0143
Carlos Ismael Terreno <carlosismaelterreno@hotmail.com>
2020-09-01 22:23:33 -0300

    Updates

---
+++ precache-manifest.4c50f1f905f1e5f92806f9aa94ff2e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ec5dfbfcc5a9b6a822377d0fd2aa84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5f4fe356abcb5ada407",
+    "url": "./static/css/main.7dfddb70.chunk.css"
+  },
+  {
+    "revision": "784b941d032452ea8887",
+    "url": "./static/js/2.c4c6b2e7.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "./static/js/2.c4c6b2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f4fe356abcb5ada407",
+    "url": "./static/js/main.9703027b.chunk.js"
+  },
+  {
+    "revision": "fb9d8328a5b4eca9aed1",
+    "url": "./static/js/runtime-main.759c2fd9.js"
+  }
+]);
